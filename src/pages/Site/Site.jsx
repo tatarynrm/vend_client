3c39b3d4b5359ce203c4724c910c9c33458e08c4
tatarynrm <tatarynrm@gmail.com>
2024-04-18 17:17:04 +0300

    ewqewq

--- src/pages/Site/Site.jsx
+++ src/pages/Site/Site.jsx
@@ -5,26 +5,57 @@ import SiteMachineItem from "./SiteMachineItem";
 
 const Site = () => {
   const [machines, setMachines] = useState([]);
-
+  const [contactBtn,setContactBtn] = useState({})
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setContactBtn((prevFormData) => ({
+      ...prevFormData,
+      [name]: value.trim(),
+    }));
+  };
+  const getMachines = async () => {
+    try {
+      const { data } = await axios("https://api.vendmarket.space/site");
+      setMachines(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const getComntacts = async () => {
+    try {
+      const { data } = await axios("https://api.vendmarket.space/site/contacts");
+      console.log(data);
+      setContactBtn(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const updatePhone = async () => {
+    try {
+      const data = await axios.post(
+        "https://api.vendmarket.space/site/update-number",
+        contactBtn
+      );
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   useEffect(() => {
-    const getMachines = async () => {
-      try {
-        const { data } = await axios("https://api.vendmarket.space/site");
-        console.log(data);
-        setMachines(data);
 
-      } catch (error) {
-        console.log(error);
-      }
-    };
     getMachines();
+    getComntacts()
   }, []);
 
   return (
     <div className="site page">
-       <Box display={'flex'} justifyContent={'center'}>
-       <Text fontWeight={'bold'}>Адміністрування сайтом vendwater.com.ua</Text>
-       </Box>
+      <Box display={"flex"} justifyContent={"center"}>
+        <Text fontWeight={"bold"}>Адміністрування сайтом vendwater.com.ua</Text>
+      </Box>
+      <Box className="container" display={'flex'} gap={10}>
+        <Input name="phone_number" placeholder="Вкажіть актуальний номер телефону" width={'20%'} value={contactBtn.phone_number} onChange={handleInputChange} />
+        <Button colorScheme="blue" onClick={updatePhone}>Змінити номер</Button>
+      </Box>
       <Flex
         flexDirection={"column"}
         gap={10}
